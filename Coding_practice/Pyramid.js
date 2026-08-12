a84c44841bc612a84298027fee6fa35db8186589
vishalsatpute05 <vishalsatpute2605@gmail.com>
2026-08-12 12:21:11 +0530

    Update Pyramid practice file

--- Coding_practice/Pyramid.js
+++ Coding_practice/Pyramid.js
@@ -1,7 +1,20 @@
-const n=5;
-for (let i = 1; i <= n; i++) 
-    {
-    let spaces = ' '.repeat(n - i);
-    let stars =  '*'.repeat(2 * i - 1);
-    console.log(spaces + stars);
+// const n=5;
+// for (let i = 1; i <= n; i++) 
+//     {
+//     let spaces = ' '.repeat(n - i);
+//     let stars =  '*'.repeat(2 * i - 1);
+//     console.log(spaces + stars);
+// }
+
+
+let n = 5;
+for(let i=1;i<=n;i++){
+        let row = "";
+        for(let j=1;j<=n-i;j++){
+               row += " ";
+        }
+        for(let j=1;j<=2*i-1;j++){
+            row = row + "*";
+        }
+        console.log(row);
 }
